ab0152b83aad6d2a28f13f028e67d69b9a1c5134
America-Miguel-Parra <isc19350307@gmail.com>
2023-03-28 10:00:06 -0600

    Probando los diferentes Inputs que hay

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,25 @@ function App() {
   const [InputValue, setInputValue] = useState({
     input1:"",
     input2:"",
+    password:"",
+    checkbox:"",
+    color:"",
+    date:"",
+    datetime_local:"",
+    email:"",
+    file:"",
+    hidden:"",
+    image:"",
+    month:"",
+    number:"",
+    radio:"",
+    range:"",
+    reset:"",
+    search:"",
+    tel:"",
+    time:"",
+    url:"",
+    week:"",
   });
   const handleInputChange = (e) => {
     setInputValue({
@@ -34,6 +53,253 @@ function App() {
           onChange={(e) => handleInputChange(e)}
           type="text"
           value={InputValue.input2}
+          />
+        </label>
+
+        <br/>
+       
+        <label>
+          Password :
+          <input
+          className="mt-2"
+          name = "password"
+          onChange={(e) => handleInputChange(e)}
+          type="password"
+          value={InputValue.password}
+          />
+        </label>
+
+        <br/>
+       
+        <label>
+          Checkbox 
+          <input
+          className="mt-2"
+          name = "checkbox"
+          onChange={(e) => handleInputChange(e)}
+          type="checkbox"
+          value={InputValue.checkbox}
+          />
+        </label>
+
+        <br/>
+       
+        <label>
+          Color 
+          <input
+          className="mt-2"
+          name = "color"
+          onChange={(e) => handleInputChange(e)}
+          type="color"
+          value={InputValue.color}
+          />
+        </label>
+
+        <br/>
+       
+        <label>
+          Date 
+          <input
+          className="mt-2"
+          name = "date"
+          onChange={(e) => handleInputChange(e)}
+          type="date"
+          value={InputValue.date}
+          />
+        </label>
+
+        <br/>
+       
+        <label>
+          Datetime-local 
+          <input
+          className="mt-2"
+          name = "datetime-local"
+          onChange={(e) => handleInputChange(e)}
+          type="datetime-local"
+          value={InputValue.datetime_local}
+          />
+        </label>
+
+        <br/>
+       
+        <label>
+          Email 
+          <input
+          className="mt-2"
+          name = "email"
+          onChange={(e) => handleInputChange(e)}
+          type="email"
+          value={InputValue.email}
+          />
+        </label>
+
+        <br/>
+       
+        <label>
+          File 
+          <input
+          className="mt-2"
+          name = "file"
+          onChange={(e) => handleInputChange(e)}
+          type="file"
+          value={InputValue.file}
+          />
+        </label>
+
+        <br/>
+       
+        <label>
+          Hidden 
+          <input
+          className="mt-2"
+          name = "hidden"
+          onChange={(e) => handleInputChange(e)}
+          type="hidden"
+          value={InputValue.hidden}
+          />
+        </label>
+
+        <br/>
+       
+        <label>
+          Image 
+          <input
+          className="mt-2"
+          name = "image"
+          onChange={(e) => handleInputChange(e)}
+          type="image"
+          value={InputValue.image}
+          />
+        </label>
+
+        <br/>
+       
+        <label>
+          Month 
+          <input
+          className="mt-2"
+          name = "month"
+          onChange={(e) => handleInputChange(e)}
+          type="month"
+          value={InputValue.month}
+          />
+        </label>
+
+        <br/>
+       
+        <label>
+          Number 
+          <input
+          className="mt-2"
+          name = "number"
+          onChange={(e) => handleInputChange(e)}
+          type="number"
+          value={InputValue.number}
+          />
+        </label>
+
+        <br/>
+       
+        <label>
+          Radio 
+          <input
+          className="mt-2"
+          name = "radio"
+          onChange={(e) => handleInputChange(e)}
+          type="radio"
+          value={InputValue.radio}
+          />
+        </label>
+
+        <br/>
+       
+        <label>
+          Range 
+          <input
+          className="mt-2"
+          name = "range"
+          onChange={(e) => handleInputChange(e)}
+          type="range"
+          value={InputValue.range}
+          />
+        </label>
+
+        <br/>
+       
+        <label>
+          Reset 
+          <input
+          className="mt-2"
+          name = "reset"
+          onChange={(e) => handleInputChange(e)}
+          type="reset"
+          value={InputValue.reset}
+          />
+        </label>
+
+        <br/>
+       
+        <label>
+          Search 
+          <input
+          className="mt-2"
+          name = "search"
+          onChange={(e) => handleInputChange(e)}
+          type="search"
+          value={InputValue.search}
+          />
+        </label>
+
+        <br/>
+       
+        <label>
+          Tel 
+          <input
+          className="mt-2"
+          name = "tel"
+          onChange={(e) => handleInputChange(e)}
+          type="tel"
+          value={InputValue.tel}
+          />
+        </label>
+
+        <br/>
+       
+        <label>
+          Time 
+          <input
+          className="mt-2"
+          name = "time"
+          onChange={(e) => handleInputChange(e)}
+          type="time"
+          value={InputValue.time}
+          />
+        </label>
+
+        <br/>
+       
+        <label>
+          url 
+          <input
+          className="mt-2"
+          name = "url"
+          onChange={(e) => handleInputChange(e)}
+          type="url"
+          value={InputValue.url}
+          />
+        </label>
+
+        <br/>
+       
+        <label>
+          Week 
+          <input
+          className="mt-2"
+          name = "week"
+          onChange={(e) => handleInputChange(e)}
+          type="week"
+          value={InputValue.week}
           />
         </label>
 
